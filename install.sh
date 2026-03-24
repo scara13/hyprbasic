dfec6fb093c57506070c4498d8c5805111750364
Hizumin <yoruyt01@gmail.com>
2026-03-24 18:33:32 +0800

    Fix install script

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ cd paru
 makepkg -si
 
 # Install necessary packages
-sudo pacman -S waybar fuzzel mako thunar thunar-archive-plugin fastfetch
+sudo pacman -S waybar fuzzel mako thunar thunar-archive-plugin fastfetch alacritty
 paru -S matugen-bin sunsetr-bin
 
 # Transfer configs
-cp -r .config/fastfetch .config/fuzzel .config/kitty .config/mako .config/matugen .config/sunsetr .config/waybar ~/.config/
+cp -r .config/fastfetch .config/fuzzel .config/alacritty .config/mako .config/matugen .config/sunsetr .config/waybar ~/.config/
 
-mkdir -r ~/.cache/minimalist_dotfiles/backups
-zip -rm ~/.cache/minimalist_dotfiles/backups/old_hyprland_configs.zip ~/.config/hypr/*
+mkdir -r ~/.cache/hyprbasic/backups
+zip -rm ~/.cache/hyprbasics/backups/old_hyprland_configs.zip ~/.config/hypr/*
 cp .config/hypr/* ~/.config/hypr/
 
 echo "Reboot your system."
