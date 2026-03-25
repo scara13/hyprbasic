075312591d03fad12f388f96cc4d883e3ecd0cb2
Hizumin <yoruyt01@gmail.com>
2026-03-25 10:08:33 +0800

    Update installation script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd paru
 makepkg -si
 
 # Install necessary packages
-sudo pacman -S waybar fuzzel mako thunar thunar-archive-plugin fastfetch alacritty
+sudo pacman -S waybar fuzzel mako thunar thunar-archive-plugin fastfetch alacritty hyprshot
 paru -S matugen-bin sunsetr-bin
 
 # Transfer configs
@@ -16,5 +16,16 @@ cp -r .config/fastfetch .config/fuzzel .config/alacritty .config/mako .config/ma
 mkdir -r ~/.cache/hyprbasic/backups
 zip -rm ~/.cache/hyprbasics/backups/old_hyprland_configs.zip ~/.config/hypr/*
 cp .config/hypr/* ~/.config/hypr/
+
+read -p "Include .bashrc? (y/n): " bashrc
+read -p "Include .vimrc? (y/n): " vimrc
+
+if [ "$bashrc" = "y" ]; then
+	cp -i .bashrc ~/
+fi
+
+if [ "$vimrc" = "y" ]; then
+	cp -i .vimrc ~/
+fi
 
 echo "Reboot your system."
